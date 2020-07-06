9fbd856ac947738bda80d6ceda7e306d07e6f659
Shane Weisz <shaneweisz@gmail.com>
2020-07-07 00:09:34 +0200

    Refactored solutions

--- Level 1/Prison Labor Dodgers/solution.py
+++ Level 1/Prison Labor Dodgers/solution.py
@@ -1,6 +1,10 @@
+#####################
+##### Solution ######
+#####################
+
 def solution(x, y):
-    # 1. Ensure x is the bigger of the two lists
-    #    so that x contains the additional ID
+    # 1. Ensure x is the bigger of the two lists so
+    #    that x always contains the additional ID
     if len(x) < len(y):
         x, y = y, x
 
@@ -9,8 +13,8 @@ def solution(x, y):
     y.sort()
 
     # 3. Compare x_i and y_i where i runs from 1 to n := len(y).
-    #    If x_i != y_i, it follows since the lists are sorted
-    #    and x contains the additional ID, that x_i must be the
+    #    If x_i != y_i, it follows (since the lists are sorted
+    #    and x contains the additional ID) that x_i must be the
     #    additional ID.
     for x_i, y_i in zip(x, y):
         if x_i != y_i:
@@ -20,10 +24,26 @@ def solution(x, y):
     #    (n+1)-th element of x must be the additional ID
     return x_i[-1]
 
+#####################
+#### Test Cases #####
+#####################
 
-# Test Case 1:
-print(solution([13, 5, 6, 2, 5], [5, 2, 5, 13]) == 6)
 
-# Test Case 2:
-print(solution([14, 27, 1, 4, 2, 50, 3, 1],
-               [2, 4, -4, 3, 1, 1, 14, 27, 50]) == -4)
+inputs = [([13, 5, 6, 2, 5], [5, 2, 5, 13]), ([14, 27, 1, 4, 2, 50, 3, 1],
+                                              [2, 4, -4, 3, 1, 1, 14, 27, 50])]
+expected_outputs = [6, -4]
+
+
+def check_test_case(i, inp, exp_out):
+    print("-"*20)
+    print("Test Case", i)
+
+    out = solution(*inp)
+    print("Output:", out)
+    print("Expected:", exp_out)
+
+    print("PASSED" if out == exp_out else "FAILED")
+
+
+for i, (inp, exp_out) in enumerate(zip(inputs, expected_outputs), start=1):
+    check_test_case(i, inp, exp_out)
--- Level 2/Numbers Station Coded Messages/solution.py
+++ Level 2/Numbers Station Coded Messages/solution.py
@@ -1,13 +1,20 @@
+#####################
+##### Solution ######
+#####################
+
 def solution(l, t):
+    '''
+    * Keep a running sum of the elements starting from l[i]
+    * When the sum is >= `t` or the end of the list
+        is reached, check if the running sum equals `t`
+      If not, move on to the next element of the list, and start
+        a running sum from there
+    * If none of the running sums equals `t`, then no sub-list can be summed
+        to give `t`, so return [-1,-1]
+    '''
     n = len(l)
 
     for i in range(len(l)):
-
-        #  * Keep a running sum of the elements starting from l[i]
-        #  * When the sum is >= `t` or the end of the list
-        #    is reached, check if the running sum equals `t`
-        #  * If not, move on to the next element of the list, and start
-        #    a running sum from there
 
         total = l[i]
 
@@ -19,20 +26,27 @@ def solution(l, t):
         if total == t:
             return [i, j-1]
 
-    # If none of the running sums equals `t`, then no sub-list can be summed
-    # to give `t`, so return [-1,-1]
     return [-1, -1]
 
 
-print("-"*20)
-print("Test Case 1")
-print("Output:", solution([1, 2, 3, 4], 15))
-print("Expected: [-1, -1]")
-print(solution([1, 2, 3, 4], 15) == [-1, -1])
+#####################
+#### Test Cases #####
+#####################
 
-print("-"*20)
-print("Test Case 2")
-print("Output:", solution([4, 3, 10, 2, 8], 12))
-print("Expected: [2,3]")
-print(solution([4, 3, 10, 2, 8], 12) == [2, 3])
-print("-"*20)
+inputs = [([1, 2, 3, 4], 15), ([4, 3, 10, 2, 8], 12)]
+expected_outputs = [[-1, -1], [2, 3]]
+
+
+def check_test_case(i, inp, exp_out):
+    print("-"*20)
+    print("Test Case", i)
+
+    out = solution(*inp)
+    print("Output:", out)
+    print("Expected:", exp_out)
+
+    print("PASSED" if out == exp_out else "FAILED")
+
+
+for i, (inp, exp_out) in enumerate(zip(inputs, expected_outputs), start=1):
+    check_test_case(i, inp, exp_out)
