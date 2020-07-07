3249d49cb14ced34b1eb8745145804216fa1a88b
Shane Weisz <shaneweisz@gmail.com>
2020-07-07 20:36:30 +0200

    Cleaned up Find the Access Code

--- Level 3/Challenge 2 - Find the Access Code/solution.py
+++ Level 3/Challenge 2 - Find the Access Code/solution.py
@@ -1,24 +1,34 @@
 #####################
 ##### Solution ######
 #####################
-from collections import defaultdict
-
 
 def solution(l):
+    """
+    Approach:
 
+    Step 1: Create a dictionary with i : js key-value pairs, such that for each index i,
+            js is a list of the indices j (i < j) for which li divides lj.
+
+    Step 2. Then for each key i, for each j in js, use the dictionary to count the number of
+            indices k for which lj divides lk (j < k), and add this count to a running counter.
+
+    The total counter will then be the number of triples (li, lj, lk) such that li divides lj, 
+    and lj divides lk (i < j < k).
+    """
+    # Step 1
     divides_dict = dict()
-
     for i, li in enumerate(l):
         divides_dict[i] = []
-        for j in range(i+1, len(l)):
-            lj = l[j]
+        for j, lj in enumerate(l[i+1:], start=i+1):
             if lj % li == 0:
                 divides_dict[i].append(j)
 
+    # Step 2
     counter = 0
-    for k, vs in divides_dict.items():
-        for v in vs:
-            counter += len(divides_dict.get(v))
+    for i, js in divides_dict.items():
+        for j in js:
+            ks = divides_dict[j]
+            counter += len(ks)
 
     return counter
 
